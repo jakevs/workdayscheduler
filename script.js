d843096c960075662c53ef6b89714c9590c3945b
James Van Slyke <jakevs3@gmail.com>
2020-04-12 17:55:27 -0500

    got save button working

--- script.js
+++ script.js
@@ -16,26 +16,44 @@ var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss p");
 var displayTime = document.getElementById("currentDay");
 displayTime.textContent = time
 
+$(function() {
+});
 
+
+$(".saveBtn").click(function(){
+    var time = $(this).parent().attr("id"); //target the hour box
+    var value = $(this).siblings(".task").val(); //target the text box
+
+    localStorage.setItem(time, value);
+
+})
+
+$(".task").val(localStorage.getItem("nineAm"));
 
 // function putDiv() {
 //     var div1 = $("<div></div>").text("row");
 //     $(".container").after(div1);
 // };
 
-$(function() {
+// $(function() {
 
-var txt10 = $("<div></div>").text("row 10am");
-var txt11 = $("<div></div>").text("row 11am");
-var txt12 = $("<div></div>").text("row 12am");
-var txt1 = $("<div></div>").text("row 1pm");
-var txt2 = $("<div></div>").text("row 2pm");
-var txt3 = $("<div></div>").text("row 3pm");
-var txt4 = $("<div></div>").text("row 4pm");
-var txt5 = $("<div></div>").text("row 5pm");
-    $(".hour").append(txt10, txt11, txt12, txt1, txt2, txt3, txt4, txt5);
+// var txt10 = $("<div></div>").text("row 10am");
+// var txt11 = $("<div></div>").text("row 11am");
+// var txt12 = $("<div></div>").text("row 12am");
+// var txt1 = $("<div></div>").text("row 1pm");
+// var txt2 = $("<div></div>").text("row 2pm");
+// var txt3 = $("<div></div>").text("row 3pm");
+// var txt4 = $("<div></div>").text("row 4pm");
+// var txt5 = $("<div></div>").text("row 5pm");
+//     $("div1").after(txt10, txt11, txt12, txt1, txt2, txt3, txt4, txt5);
+// });   
 
-});   
+
+// localStorage.setItem("user", JSON.stringify(user));
+
+
+
+
 // $(".container").before("<div>Row1</div>");
 //     $("div").attr("class", "hour");
 //     $("div").attr("class", "row");
