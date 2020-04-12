024701a0ae1931efa7a986717997ea02d37c1a87
James Van Slyke <jakevs3@gmail.com>
2020-04-12 13:01:56 -0500

    working on rows

--- script.js
+++ script.js
@@ -13,15 +13,29 @@
 // $(document).ready(function() {};  <- will need this
 
 var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss p");
-var displayTime = document.getElementById('currentDay');
+var displayTime = document.getElementById("currentDay");
 displayTime.textContent = time
 
-function putDiv() {
-    var div1 = $("<div></div>").text("row");
-    $(".container").after(div1);
-}
 
-// $(function() {
+
+// function putDiv() {
+//     var div1 = $("<div></div>").text("row");
+//     $(".container").after(div1);
+// };
+
+$(function() {
+
+var txt10 = $("<div></div>").text("row 10am");
+var txt11 = $("<div></div>").text("row 11am");
+var txt12 = $("<div></div>").text("row 12am");
+var txt1 = $("<div></div>").text("row 1pm");
+var txt2 = $("<div></div>").text("row 2pm");
+var txt3 = $("<div></div>").text("row 3pm");
+var txt4 = $("<div></div>").text("row 4pm");
+var txt5 = $("<div></div>").text("row 5pm");
+    $(".hour").append(txt10, txt11, txt12, txt1, txt2, txt3, txt4, txt5);
+
+});   
 // $(".container").before("<div>Row1</div>");
 //     $("div").attr("class", "hour");
 //     $("div").attr("class", "row");
@@ -33,8 +47,7 @@ function putDiv() {
 // $(".container").before("<div>Row5</div>");
 // $(".container").before("<div>Row6</div>");
 // $(".container").before("<div>Row7</div>");
-// $(".container").after("<div>Row8</div>");
-// });
+// $(".container").after("<div></div>");
 
 
 
